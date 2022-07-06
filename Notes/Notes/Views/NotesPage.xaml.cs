4af89326e549d107014bd5e371fb131bbe0f100f
Sern_Intern <Sern_Intern@SERN-INTERN53.SERN.MIL>
2022-07-06 11:11:22 -0400

    added Notes

---
+++ Notes/Notes/Views/NotesPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using Xamarin.Forms;
+
+namespace Notes.Views
+{
+    public partial class NotesPage : ContentPage
+    {
+        string _fileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "notes.txt");
+
+        public NotesPage()
+        {
+            InitializeComponent();
+
+            
+           
+        }
+
+        void OnSaveButtonClicked(object sender, EventArgs e)
+        {
+          
+        }
+
+        void OnDeleteButtonClicked(object sender, EventArgs e)
+        {
+            // Delete the file.
+            if (File.Exists(_fileName))
+            {
+                File.Delete(_fileName);
+            }
+           
+        }
+    }
+}
