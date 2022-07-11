5068eccfb8cf1e9cef4b2fc107e7d3137105e8be
Brandon Boone <Brandonboone904@gmail.com>
2022-07-11 13:29:12 -0400

    bruhhhh

--- Notes/Notes/Views/Location.xaml.cs
+++ Notes/Notes/Views/Location.xaml.cs
@@ -6,7 +6,6 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
-using HtmlAgilityPack;
 
 namespace Notes.Views
 {
@@ -16,18 +15,6 @@ namespace Notes.Views
         public Location()
         {
             InitializeComponent();
-        }
-    }
-    class Program
-    {
-        static void Main(String[] args)
-        {
-            HtmlAgilityPack.HtmlWeb web = new HtmlAgilityPack.HtmlWeb;
-            HtmlAgilityPack.HtmlDocument doc = web.Load("https://www.weatherbug.com/weather-forecast/now/robins-air-force-base-ga-31098");
-            foreach (var item in doc.DocumentNode.SelectNodes("/html/body/main/section[1]/div[2]/div/div[2]/div/div[2]/div/div/div/div/div/div/div/div/ul/li[1]/div/div/div/current-conditions-widget/div/div/div[1]"))
-            {
-
-            }
         }
     }
 }
