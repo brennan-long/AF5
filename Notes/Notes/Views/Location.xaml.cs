ccfc0ffe2706c404c6722d63b018933f28909f04
Brandon Boone <Brandonboone904@gmail.com>
2022-07-11 14:50:01 -0400

    noahs 3rdc ommit

--- Notes/Notes/Views/Location.xaml.cs
+++ Notes/Notes/Views/Location.xaml.cs
@@ -18,16 +18,5 @@ namespace Notes.Views
             InitializeComponent();
         }
     }
-    class Program
-    {
-        static void Main(String[] args)
-        {
-            HtmlAgilityPack.HtmlWeb web = new HtmlAgilityPack.HtmlWeb;
-            HtmlAgilityPack.HtmlDocument doc = web.Load("https://www.weatherbug.com/weather-forecast/now/robins-air-force-base-ga-31098");
-            foreach (var item in doc.DocumentNode.SelectNodes("/html/body/main/section[1]/div[2]/div/div[2]/div/div[2]/div/div/div/div/div/div/div/div/ul/li[1]/div/div/div/current-conditions-widget/div/div/div[1]"))
-            {
-
-            }
-        }
-    }
+   
 }
