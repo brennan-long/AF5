6574eb7743c76106a0e18e5f00906b5c3ded4bbb
Brandon Boone <Brandonboone904@gmail.com>
2022-07-12 15:28:10 -0400

    this should work

--- Notes/Notes/Views/Location.xaml.cs
+++ Notes/Notes/Views/Location.xaml.cs
@@ -21,8 +21,26 @@ namespace Notes.Views
         public Location()
         {
             InitializeComponent();
+<<<<<<< HEAD
 
         }
 
     }
 }
+=======
+        }
+            public static class lol
+        {
+            public static void run()
+            {
+                HtmlWeb web = new HtmlWeb();
+                HtmlDocument doc = web.Load("https://www.airnav.com/airport/KHIF");
+                var hilldim1 = doc.DocumentNode.SelectNodes("/html/body/table[5]/tr/td[1]/table[6]/tr[1]/td[2]")[0].InnerText;
+
+            }
+        }
+    }
+}
+    
+    
+>>>>>>> 82d2926a7e0f04d3949ad5444c08ecfba6fb4b87
