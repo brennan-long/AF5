e359986793f654594e02aa3e6bb4c47e8d94dd17
Brandon-Boone <108550829+Brandon-Boone@users.noreply.github.com>
2022-07-12 15:43:35 -0400

    Omgg (#18)
    * noahs 3rdc ommit
    
    * naohs commit day 2
    
    * this sucks
    
    * this should work

--- Notes/Notes/Views/Location.xaml.cs
+++ Notes/Notes/Views/Location.xaml.cs
@@ -21,6 +21,14 @@ namespace Notes.Views
         public Location()
         {
             InitializeComponent();
+
+
+
+        }
+
+    }
+}
+=======
         }
             public static class lol
         {
@@ -36,3 +44,21 @@ namespace Notes.Views
 }
     
     
+
+=======
+        }
+            public static class lol
+        {
+            public static void run()
+            {
+                HtmlWeb web = new HtmlWeb();
+                HtmlDocument doc = web.Load("https://www.airnav.com/airport/KHIF");
+                var hilldim1 = doc.DocumentNode.SelectNodes("/html/body/table[5]/tr/td[1]/table[6]/tr[1]/td[2]")[0].InnerText;
+
+            }
+        }
+    }
+}
+    
+    
+
