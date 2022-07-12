4b0cdbe79e6a3ac002536e9fb0acc1e3d9693430
Brennan Long <brennan.long04@gmail.com>
2022-07-12 15:03:04 -0400

    DONT BREAK

--- Notes/Notes/AssemblyInfo.cs
+++ Notes/Notes/AssemblyInfo.cs
@@ -2,7 +2,3 @@ using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
-
-[assembly: ExportFont("Font Awesome 6 Free-Solid-900.otf", Alias ="FS")]
-[assembly: ExportFont("Font Awesome 6 Free-Regular-900.otf", Alias = "")]
-[assembly: ExportFont("Font Awesome 6 Brands-Regular-900.otf", Alias = "")]
