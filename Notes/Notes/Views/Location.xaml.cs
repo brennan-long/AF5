a3a69588c45e3210facd864da68bf98aeb3f9348
Brandon-Boone <108550829+Brandon-Boone@users.noreply.github.com>
2022-07-12 12:34:44 -0400

    Calculations (#13)
    * calculations and other stuff
    
    * i hope this works
    
    * bruhhhh
    
    * Noahs commit test
    
    * noah other commit
    
    * remove files?
    
    * removed more files?
    
    Co-authored-by: Brennan Long <brennan.long04@gmail.com>
    Co-authored-by: brennan-long <108550828+brennan-long@users.noreply.github.com>

--- Notes/Notes/Views/Location.xaml.cs
+++ Notes/Notes/Views/Location.xaml.cs
@@ -6,7 +6,6 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
-using HtmlAgilityPack;
 
 namespace Notes.Views
 {
@@ -18,15 +17,6 @@ namespace Notes.Views
             InitializeComponent();
            
         }
-    } 
-    public static class lol
-    {
-        public static void run()
-        {
-            HtmlWeb web = new HtmlWeb();
-            HtmlDocument doc = web.Load("https://www.airnav.com/airport/KHIF");
-            var hilldim1 = doc.DocumentNode.SelectNodes("/html/body/table[5]/tr/td[1]/table[6]/tr[1]/td[2]")[0].InnerText;
-
-        }
     }
 }
+
