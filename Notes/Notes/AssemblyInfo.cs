5b96985ad0b69b27c9e28e208a2fd20f6dceddfb
brennan-long <108550828+brennan-long@users.noreply.github.com>
2022-07-12 13:03:10 -0400

    Idk man (#14)

--- Notes/Notes/AssemblyInfo.cs
+++ Notes/Notes/AssemblyInfo.cs
@@ -3,6 +3,6 @@ using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 
-[assembly: ExportFont("Font Awesome 6 Free-Solid-900.otf", Alias ="FS")]
-[assembly: ExportFont("Font Awesome 6 Free-Regular-900.otf", Alias = "")]
-[assembly: ExportFont("Font Awesome 6 Brands-Regular-900.otf", Alias = "")]
+[assembly: ExportFont("Font Awesome 6 Free-Solid-900.otf", Alias = "FS")]
+[assembly: ExportFont("Font Awesome 6 Free-Regular-900.otf", Alias = "FR")]
+[assembly: ExportFont("Font Awesome 6 Brands-Regular-900.otf", Alias = "BR")]
