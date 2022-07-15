6fb8fe01be60e989a614ab2d543d99aa355db769
Brandon-Boone <108550829+Brandon-Boone@users.noreply.github.com>
2022-07-15 14:50:28 -0400

    Weird (#25)
    * noahs 3rdc ommit
    
    * naohs commit day 2
    
    * david did this
    
    * this sucks
    
    * lets see
    
    * idk
    
    * bracket added could be an error
    
    * android porject issue
    
    * stopit pleasea
    
    * notes page stuff
    
    * notes page and stuff
    
    * i messed up
    
    * i hope this works

--- Notes/Notes/Views/AboutPage.xaml.cs
+++ Notes/Notes/Views/AboutPage.xaml.cs
@@ -6,20 +6,67 @@ namespace Notes.Views
 {
     public partial class AboutPage : ContentPage
     {
+        slotsInfo[] slots;
         public AboutPage()
         {
             InitializeComponent();
+
             aircraftWeight.Text = "this sucks";
+
+            if (Info.currentPlane != null)
+            {
+                aircraftWeight.Text = Info.currentPlane.baseweight.ToString();
+            }
+
             balanceResults.Text = "this really sucks";
             takeoffRecomendations.Text = "like really really sucks";
 
+            slots = new slotsInfo[]
+            {
+                new slotsInfo("Select", 0),
+                new slotsInfo("Humvee", 7700),
+                new slotsInfo("M1 Tank", 120000),
+                new slotsInfo("UH-60 Blackhawk", 13650),
+                new slotsInfo("Stryker ICV", 32940),
+                new slotsInfo("CV90", 54000)
+            };
+
+            picker1.ItemsSource = slots;
+            picker2.ItemsSource = slots;
+            picker3.ItemsSource = slots;
+            picker4.ItemsSource = slots;
+            picker5.ItemsSource = slots;
+        }
+        private void picker_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int weight = (picker1.SelectedItem as slotsInfo).weight;
+            aircraftWeight.Text = weight.ToString();
         }
 
         async void OnButtonClicked(object sender, EventArgs e)
         {
             // Launch the specified URL in the system browser.
             await Launcher.OpenAsync("https://en.wikipedia.org/wiki/Trollface");
-        }      
+        }
+    }
+    public class slotsInfo
+    {
+        public string name;
+        public int weight;
+
+        public slotsInfo(string n, int w)
+        {
+            this.name = n;
+            this.weight = w;
+        }
+        public override string ToString()
+        {
+            return name;
+        }
     }
 }
+
+
+
+
 
--- Notes/Notes/Views/Location.xaml.cs
+++ Notes/Notes/Views/Location.xaml.cs
@@ -21,22 +21,29 @@ namespace Notes.Views
         public Location()
         {
             InitializeComponent();
-        } 
 
-        public class airportInfo
-        {
-            public string name;
-            public string runwayinfo;
-        public airportInfo (string AIname, string AIrunway)
-            {
-                this.name = AIname;
-                this.runwayinfo = AIrunway;
-            }
+
         }
-        public class runwayInfo
+
+    }
+
+
+
+
+    public class airportInfo
+    {
+        public string name;
+        public string runwayinfo;
+        public airportInfo(string AIname, string AIrunway)
         {
-            public string 
+            this.name = AIname;
+            this.runwayinfo = AIrunway;
         }
+    }
+    public class runwayInfo
+    {
+        public string dee;
+    }
         
         
          public static class lol
