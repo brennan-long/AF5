b6e5b06481ae12907b04d2f14bb6dce7f7b40c40
Brandon Boone <Brandonboone904@gmail.com>
2022-07-25 14:38:12 -0400

    locationstuff

---
+++ Notes/Notes/Runway.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Notes
+{
+    
+
+    internal class Runway
+    {
+        string name;
+    }
+}
--- Notes/Notes/Views/Location.xaml.cs
+++ Notes/Notes/Views/Location.xaml.cs
@@ -18,17 +18,17 @@ namespace Notes.Views
         public Location()
         {
             InitializeComponent();
-
+           
             locationPicker.ItemsSource = new String[] { };
             
 
            airportInfo[] airports = new airportInfo[]   
             {
-                new airportInfo("Robins Air Force Base", "Runway 1"),
-                new airportInfo ("Hill Air Force Base", "Runway 2"),
-                new airportInfo("Tinker Air Force Base", "Runway 3"),
-                new airportInfo ("Shaw Air Force Base", "Runway 4"),
-                new airportInfo ("Hartsfield-Jackson International Airport", "Runway 4"),
+                new airportInfo("Robins Air Force Base", "Runway 1", ""),
+                new airportInfo ("Hill Air Force Base", "Runway 2", ""),
+                new airportInfo("Tinker Air Force Base", "Runway 3", ""),
+                new airportInfo ("Shaw Air Force Base", "Runway 4", ""),
+                new airportInfo ("Hartsfield-Jackson International Airport", "Runway 4", ""),
             };
             locationPicker.SelectedItem = airports[0];
             //locationPicker.ItemsSource = airports;
@@ -48,10 +48,23 @@ namespace Notes.Views
     {
         public string name;
         public string runwayinfo;
-        public airportInfo(string AIname, string AIrunway)
+        public airportInfo(string AIname, string AIrunway, string AirportID)
         {
             this.name = AIname;
             this.runwayinfo = AIrunway;
+            string XRoot = "/html/body/table[5]/tr/td[1]";
+            string url = "https://www.airnav.com/airport/" + AirportID;
+            bool runway = true;
+            int i = 1;
+            while (runway)
+            {
+                string xpath = XRoot + "h4[" + i.ToString() + "]";
+                if (WebScrape(xpath, url) != null)
+                {
+
+                }
+            }
+            string info = WebScrape(, url);
         }
 
         public airportInfo(string AIname)
@@ -72,11 +85,20 @@ namespace Notes.Views
         }
         public void Setrunwayinfo(string AIrunway)
         {
-            runwayinfo= AIrunway;
+            runwayinfo = AIrunway;
+        }
+        public HtmlNode WebScrape(string xpath, string url)
+        {
+            HtmlWeb web = new HtmlWeb();
+            HtmlDocument doc = web.Load(url);
+            return doc.DocumentNode.SelectNodes(xpath)[0];
+
         }
 
-    }
 
+
+    }
+   
         /* public static class lol
 
         {
